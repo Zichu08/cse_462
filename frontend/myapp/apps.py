16f6561e347efb48ab0ad86a835c49c7fff73e76
Weikai Rao <weikairao@gmail.com>
2025-04-02 23:22:46 -0500

    Update apps.py

--- frontend/myapp/apps.py
+++ frontend/myapp/apps.py
@@ -17,9 +17,9 @@ class MyappConfig(AppConfig):
 
         print("[DEBUG] MyAppConfig.ready(): Loading filter overlay in main thread.")
         from pynq import Overlay
+        overlay = Overlay("/home/xilinx/pynq/overlays/filter/filter.bit")
 
-        filter_overlay = Overlay("/home/xilinx/pynq/overlays/filter/filter.bit")
-        hardware_globals.overlay = filter_overlay
+        hardware_globals.filter_overlay = overlay
         hardware_globals.filter_dma = filter_overlay.axi_dma_0
         hardware_globals.filter_kernel_ip = filter_overlay.filter_kernel_0
         print("[DEBUG] Overlay loaded and references stored in hardware_globals.")
